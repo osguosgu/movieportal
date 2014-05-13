1a8c5467d22a7f275d29cf796266748675d84968
Luotola Lauri <lauri.luotola@aalto.fi>
2014-05-13 20:24:04 +0300

    added route to get current user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -64,7 +64,11 @@ class UsersController < ApplicationController
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_user
-      @user = User.find(params[:id])
+      if params[:id] == 'me'
+        @user = current_user
+      else
+        @user = User.find(params[:id])
+      end
     end
 
     # Never trust parameters from the scary internet, only allow the white list through.
